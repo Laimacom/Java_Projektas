6b2cda50ba5f9c89aac7645d9a9b7ecb3252066e
Laimacom <laima.grivaciauskaite@gmail.com>
2018-11-14 10:21:15 +0200

    sss

---
+++ src/Uzdavinys.java
@@ -0,0 +1,12 @@
+
+public class Uzdavinys {
+	
+	public static void main(String[] args) {
+		int sk1 = 3;
+		int sk2 = 5;
+		int sk3 = 6;
+		int ats = sk1 + sk2 + sk3;
+		System.out.println("atsakymas: " + ats);
+		}
+	
+}
