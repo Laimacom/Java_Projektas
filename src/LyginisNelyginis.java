802d4f337ea78766cafb7feef7acbab9b970f20c
Laimacom <laima.grivaciauskaite@gmail.com>
2018-11-20 15:03:52 +0200

    11 2o

--- src/LyginisNelyginis.java
+++ src/LyginisNelyginis.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 public class LyginisNelyginis {
+	
 	public static void main(String[] args) { 
-	       
-	      Scanner reader= new Scanner(System.in);
-	         System.out.println("Iveskite sveika skaiciu:");
-	         int sk= reader.nextInt();
-	         if (sk % 2 == 0) {
+		System.out.println("Iveskite sveika skaiciu:");
+		Scanner reader= new Scanner(System.in);
+		int sk= reader.nextInt();
+		
+		lyginisNelyginisSprendimas(sk);
+		
+		reader.close();
+		}
+	
+	private static void lyginisNelyginisSprendimas(int a) {
+		
+	         if (a % 2 == 0) {
 	        	 System.out.println("Lyginis"); 
 	         } else {
 	        	 System.out.println("Nelyginis"); 
 	         }
-	         reader.close();
-	   }
+	  	}
 }
 
--- src/Plotai.java
+++ src/Plotai.java
@@ -10,31 +10,52 @@ public class Plotai {
 		System.out.println("Apskritimas - 3");
 		System.out.println("Pasirinkite, kurios figuros plota norite skaiciuoti (nurodykite skaiciu): ");
 		int skaicius = reader.nextInt();
-		
 		if (skaicius == 1) {
 			System.out.println("Iveskite pirmos krastines ilgi: ");
 			int krastine1 = reader.nextInt();
 			System.out.println("Iveskite antros krastines ilgi: ");
 			int krastine2 = reader.nextInt();
-			int trikampioPlotas = (krastine1*krastine2)/2;
-			System.out.println("Trikampio plotas: " + trikampioPlotas);
+	
+			double ats = trikampioPlotas(krastine1, krastine2);
+			System.out.println("Trikampio plotas: " + ats);
+			
 		} else if (skaicius == 2) {
 			System.out.println("Iveskite staciakampio ilgi: ");
 			int ilgis = reader.nextInt();
 			System.out.println("Iveskite staciakampio ploti: ");
 			int plotis = reader.nextInt();
-			int staciakampioPlotas = ilgis*plotis;
-			System.out.println("StaciakampioPlotas: " + staciakampioPlotas);			
+			
+			double ats = staciakampioPlotas(ilgis, plotis);
+			System.out.println("Staciakampio plotas: " + ats);	
+			
 		} else if (skaicius == 3) {
 			System.out.println("Iveskite apskritimo spinduli: ");
-			int r = reader.nextInt();
-			double apskritimoPlotas = Math.pow(r, 2)*Math.PI;
-			System.out.println("StaciakampioPlotas: " + apskritimoPlotas);				
+			double r = reader.nextInt();
+			
+			double ats = apskritimoPlotas(r);
+			System.out.println("Apskritimo plotas: " + ats);
+			
 		} else {
 			System.out.println("Tokio pasirinkimo nera. Nurodytas neteisingas skaicius");
 		}
 		
 		reader.close();
-
+	}
+		
+	private static int trikampioPlotas(int a, int b) {
+		int plotas = 0;
+		plotas = (a * b)/2;	
+		return plotas;
+	}
+	
+	private static int staciakampioPlotas(int a, int b) {
+		int plotas = 0;
+		plotas = a * b;	
+		return plotas;
+	}
+	private static double apskritimoPlotas(double a) {
+		double plotas = 0;
+		plotas = Math.pow(a, 2)*Math.PI;	
+		return plotas;
 	}
 }
